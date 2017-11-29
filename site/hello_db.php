8427b1c4a9200e57a322c006e2a66ac1224d84f8
Katy <katy.keating@lessannoyingcrm.com>
2017-11-29 10:56:12 -0600

    Copied over html files

--- site/hello_db.php
+++ site/hello_db.php
@@ -1,6 +1,6 @@
 <?php
 include('init.php');
 
-$result = dbQuery("SELECT * FROM messages limit 1");
+$result = dbQuery("SELECT * FROM messages limit 1")->fetch();
 
 echo "<h1>".$result['MessageBody']."</h1>";
