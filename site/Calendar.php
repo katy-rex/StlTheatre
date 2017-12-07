7181202f96775fe1cc9f87887e3a5e6184172935
Katy <katy.keating@lessannoyingcrm.com>
2017-12-07 09:37:36 -0600

    Some updates
    Figuring out the nuances of branches, and I gotta commit to find something else.

--- site/Calendar.php
+++ site/Calendar.php
@@ -1,29 +1,10 @@
 <html>
-	<head>
-	<link rel="stylesheet" type="text/css" href="Style.css">
-	<link href="https://fonts.googleapis.com/css?family=Raleway"
-	rel="stylesheet">
-		<title>
-			St. Louis Theatre Calendar
-		</title>
-	</head>
-	<body>
-		<div class="Header">
-			<?php include 'include/header.php';
-				PrintHeader();
+	<?php
+		include('init.php');
+		// $SiteTitle='St. Louis Theatre Calendar';
+		// $PageTitle='Calendar';
+		PrintHeader($SiteTitle='St. Louis Theatre Calendar',
+			$PageTitle='Calendar');
+		PrintFooter();
 			?>
-		</div>
-		<div class="PageTitle">
-			<h2>
-				Calendar
-			</h2>
-		</div>
-		<div class="DivideLine">
-		</div>
-		<div class="IncludeFooter">
-			<?php include 'include/footer.php';
-				PrintFooter();
-			?>
-		</div>
-	</body>
 </html>
--- site/include/footer.php
+++ site/include/footer.php
@@ -2,20 +2,21 @@
 
 function PrintFooter(){
 	echo'
-	<div class="Footer">
-		<a href="FAQ.php">
-			<div class="FooterButton">
-				<p>
-					FAQ
-				</p>
-			</div>
-		</a>
-		<a href="Contact.php">
-			<div class="FooterButton">
-				<p>
-					Contact
-				</p>
-			</div>
-		</a>
-	</div>';
+		<div class="Footer">
+			<a href="FAQ.php">
+				<div class="FooterButton">
+					<p>
+						FAQ
+					</p>
+				</div>
+			</a>
+			<a href="Contact.php">
+				<div class="FooterButton">
+					<p>
+						Contact
+					</p>
+				</div>
+			</a>
+		</div>
+	</body>';
 }
